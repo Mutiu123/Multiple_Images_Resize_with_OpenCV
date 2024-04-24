2e3777e4d81b0bd15608fba9997d8f8880983da2
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-04-24 02:29:27 +0100

    add image resized code

--- Images_Resize.py
+++ Images_Resize.py
@@ -2,6 +2,20 @@ import cv2
 import glob
 import os
 
-path = ("Computer Vision Projects/Resources")
-os.mkdir("Resized Folder")
+path = "Resources"
+os.mkdir("Resized Images")
 
+i = 0
+
+for images in glob.glob(path + "/*.jpg"):
+    image = cv2.imread(images)
+    imageResized = cv2.resize(image, (600, 600))
+    cv2.imwrite("Resized Images/image%i.jpg" %i, imageResized)
+    
+    
+    i +=1
+    cv2.imshow("image", imageResized)
+    cv2.waitKey(500)
+    
+    
+cv2.destroyAllWindows()
